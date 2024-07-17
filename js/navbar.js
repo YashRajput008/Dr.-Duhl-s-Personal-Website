eb2bc9c3a982921d2cb3fb0fb2c3f7c4457f4b56
Yash Rajput <to.yashrajput@gmail.com>
2024-07-17 19:07:42 -0400

    Made Hamburger Menu

--- js/navbar.js
+++ js/navbar.js
@@ -1,14 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const nav = document.querySelector("nav");
+window.onload = function () {
+  const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
 
-  hamburger.addEventListener("click", function () {
-    nav.classList.toggle("active");
+  menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
   });
-
-  hamburger.addEventListener("keypress", function (event) {
-    if (event.key === "Enter") {
-      nav.classList.toggle("active");
-    }
-  });
-});
+};
