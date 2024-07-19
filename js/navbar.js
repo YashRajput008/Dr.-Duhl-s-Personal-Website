11dc3d6e3202561a7360930220d958540e3c3177
Yash Rajput <to.yashrajput@gmail.com>
2024-07-18 22:34:25 -0400

    Added Underline Animation to Nav Bar

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,11 @@
 window.onload = function () {
   const menu_btn = document.querySelector(".hamburger");
   const mobile_menu = document.querySelector(".mobile-nav");
+  const active_menu_btn = document.querySelector(".active-hamburger");
 
   menu_btn.addEventListener("click", function () {
     menu_btn.classList.toggle("is-active");
     mobile_menu.classList.toggle("is-active");
+    active_menu_btn.classList.toggle("is-active");
   });
 };
